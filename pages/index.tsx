de3f6312f0cdf61b353fd3b0e86862614e73027d
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-28 17:37:24 +0530

    new favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Space Website</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.svg" />
       </Head>
       <main className="text-rose-500">Howdy</main>
     </>
