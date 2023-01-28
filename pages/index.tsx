08250edb423dce2df00e860c1ed4f2a177503026
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-28 22:18:38 +0530

    home page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,22 +38,22 @@ export default function Home() {
           className="bg-home-mobile bg-cover bg-no-repeat md:bg-home-tablet lg:bg-home-desktop"
         >
           <main className="content">
-            <section className="hero-size grid place-items-center px-6 py-24 md:px-40">
+            <section className="hero-size grid place-items-center px-6 py-24 md:px-40 md:py-52">
               <div className="max-w-5xl">
-                <div className="grid gap-20 text-center">
+                <div className="grid gap-20 text-center md:gap-36 lg:grid-cols-2">
                   <div>
                     <div>
-                      <p className="mb-5 font-barlow-condensed uppercase tracking-widest">
+                      <p className="mb-5 font-barlow-condensed uppercase tracking-widest md:mb-6 md:text-xl">
                         so, you want to trave to
                       </p>
                     </div>
                     <div>
-                      <h1 className="mb-11 font-bellefair text-7xl uppercase">
+                      <h1 className="mb-11 font-bellefair text-7xl uppercase md:mb-14 md:text-9xl">
                         space
                       </h1>
                     </div>
                     <div>
-                      <p className="font-barlow">
+                      <p className="max-w-md font-barlow lg:text-lg">
                         Let’s face it; if you want to go to space, you might as
                         well genuinely go to outer space and not hover kind of
                         on the edge of it. Well sit back, and relax because
@@ -61,10 +61,10 @@ export default function Home() {
                       </p>
                     </div>
                   </div>
-                  <div className="mx-auto">
+                  <div className="mx-auto lg:self-end">
                     <Link
                       href={"/destination"}
-                      className="grid aspect-square w-36 place-items-center rounded-full bg-white font-bellefair text-xl uppercase text-primary-500 transition-all duration-200 hover:scale-105 focus:outline-none focus:ring focus:ring-blue-500"
+                      className="grid aspect-square w-36 place-items-center rounded-full bg-white font-bellefair text-xl uppercase text-primary-500 transition-all duration-200 hover:scale-105 focus:outline-none focus:ring focus:ring-blue-500 md:w-60 md:text-3xl lg:w-72"
                     >
                       Explore
                     </Link>
