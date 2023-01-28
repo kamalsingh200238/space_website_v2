2c6f5c296aa85bdd315b70896f4f01b7690887e7
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-28 22:05:05 +0530

    main section content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Barlow, Barlow_Condensed, Bellefair } from "@next/font/google";
 import Navbar from "@/components/Navbar";
+import Link from "next/link";
 
 const barlow = Barlow({
   subsets: ["latin"],
@@ -32,6 +33,47 @@ export default function Home() {
         className={`min-h-screen bg-primary-500 text-white ${barlow.variable} ${barlowCondensed.variable} ${bellefair.variable}`}
       >
         <Navbar />
+        <div
+          id="content"
+          className="bg-home-mobile bg-cover bg-no-repeat md:bg-home-tablet lg:bg-home-desktop"
+        >
+          <main className="content">
+            <section className="hero-size grid place-items-center px-6 py-24 md:px-40">
+              <div className="max-w-5xl">
+                <div className="grid gap-20 text-center">
+                  <div>
+                    <div>
+                      <p className="mb-5 font-barlow-condensed uppercase tracking-widest">
+                        so, you want to trave to
+                      </p>
+                    </div>
+                    <div>
+                      <h1 className="mb-11 font-bellefair text-7xl uppercase">
+                        space
+                      </h1>
+                    </div>
+                    <div>
+                      <p className="font-barlow">
+                        Let’s face it; if you want to go to space, you might as
+                        well genuinely go to outer space and not hover kind of
+                        on the edge of it. Well sit back, and relax because
+                        we’ll give you a truly out of this world experience!
+                      </p>
+                    </div>
+                  </div>
+                  <div className="mx-auto">
+                    <Link
+                      href={"/destination"}
+                      className="grid aspect-square w-36 place-items-center rounded-full bg-white font-bellefair text-xl uppercase text-primary-500 transition-all duration-200 hover:scale-105 focus:outline-none focus:ring focus:ring-blue-500"
+                    >
+                      Explore
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
       </div>
     </>
   );
