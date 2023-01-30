4e1b9486205653966468fa9ccdd084317f24cf31
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-30 13:24:43 +0530

    next/font and navbar added to every page using Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+import { Barlow, Barlow_Condensed, Bellefair } from "@next/font/google";
+
+const barlow = Barlow({
+  subsets: ["latin"],
+  variable: "--barlow-font",
+  weight: ["400", "700"],
+});
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ["latin"],
+  variable: "--barlow-condensed-font",
+  weight: ["400", "700"],
+});
+
+const bellefair = Bellefair({
+  subsets: ["latin"],
+  variable: "--bellefair-font",
+  weight: ["400"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      className={`${barlow.variable} ${barlowCondensed.variable} ${bellefair.variable}`}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
