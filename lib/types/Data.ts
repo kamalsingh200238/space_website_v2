248c54291b7fa79ff89159685c1f6c6802ef10fe
Kamal Singh <kamalsingh200238@gmail.com>
2023-01-30 14:30:16 +0530

    Data type for data

---
+++ lib/types/Data.ts
@@ -0,0 +1,29 @@
+export interface Data {
+  destinations: {
+    name: string;
+    images: {
+      png: string;
+      webp: string;
+    };
+    description: string;
+    distance: string;
+    travel: string;
+  }[];
+  crew: {
+    name: string;
+    images: {
+      png: string;
+      webp: string;
+    };
+    role: string;
+    bio: string;
+  }[];
+  technology: {
+    name: string;
+    images: {
+      portrait: string;
+      landscape: string;
+    };
+    description: string;
+  }[];
+}
