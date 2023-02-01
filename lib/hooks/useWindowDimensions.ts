db6258840f2b1068537b932f4662bfd006c55600
Kamal Singh <kamalsingh200238@gmail.com>
2023-02-01 22:42:55 +0530

    hook that returns window dimensions

---
+++ lib/hooks/useWindowDimensions.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function useWindowDimensions() {
+  const hasWindow = typeof window !== "undefined";
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : 1440;
+    const height = hasWindow ? window.innerHeight : 800;
+    return {
+      width,
+      height,
+    };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize(): void {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hasWindow]);
+
+  return windowDimensions;
+}
