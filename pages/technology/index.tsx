f0fbeb9068e3d6c2bd143aa84d50811b092d8317
Kamal Singh <kamalsingh200238@gmail.com>
2023-02-02 00:05:33 +0530

    completed the page and splide styles

--- pages/technology/index.tsx
+++ pages/technology/index.tsx
@@ -40,14 +40,14 @@ export default function Technology({
     <div className="min-h-screen bg-primary-500 text-white">
       <div
         id="content"
-        className="bg-tech-mobile md:bg-tech-tablet lg:bg-tech-desktop bg-cover bg-no-repeat"
+        className="bg-technology-mobile bg-cover bg-no-repeat md:bg-technology-tablet lg:bg-technology-desktop"
       >
         <main className="">
           <section className="hero-size grid justify-center py-28 md:px-40 md:py-52">
             <div className="max-w-5xl">
               <Heading headingNumber="03" headingText="space launch 101" />
-              <div className="grid place-items-center gap-20 lg:grid-cols-2 lg:gap-56">
-                <div className="w-full">
+              <div className="grid place-items-center gap-20 lg:grid-cols-2 lg:gap-40">
+                <div className="w-full lg:order-1">
                   <Splide
                     onActive={(_splide, slide) => {
                       // when active slide changes call the function to change the value of count
@@ -61,15 +61,15 @@ export default function Technology({
                     {techData.map((singeleTech, index) => {
                       return (
                         <SplideSlide key={index}>
-                          <div className="relative aspect-[4/2] lg:aspect-square w-full">
+                          <div className="relative aspect-video w-full lg:aspect-square">
                             <Image
                               src={
-                                width < 768
+                                width < 1024
                                   ? singeleTech.images.landscape
                                   : singeleTech.images.portrait
                               }
                               alt={`${singeleTech.name} image`}
-                              className="object-contain"
+                              className="object-cover lg:object-contain"
                               fill={true}
                             />
                           </div>
@@ -78,19 +78,20 @@ export default function Technology({
                     })}
                   </Splide>
                 </div>
-                <div className="text-center lg:text-left">
+                <div className="px-6 text-center lg:text-left">
                   <div>
-                    <p className="mb-2 font-bellefair md:text-2xl lg:text-4xl">
+                    <p className="mb-2 font-barlow-condensed uppercase tracking-widest md:mb-4 ">
+                      the terminology...
+                    </p>
+                  </div>
+                  <div>
+                    <p className="mb-4 font-bellefair text-2xl uppercase md:text-4xl lg:text-6xl">
                       {activeTech.name}
                     </p>
                   </div>
                   <div>
-                    <p className="mb-4 font-bellefair text-2xl md:text-4xl lg:text-6xl">
-                      {activeTech.description}
-                    </p>
-                  </div>
-                  <div>
                     <p className="min-h-[10rem] font-barlow lg:text-lg">
+                      {activeTech.description}
                       {/* {activeTech.bio} */}
                     </p>
                   </div>
@@ -104,6 +105,15 @@ export default function Technology({
         {`
           .splide {
             padding: 0;
+          }
+          .splide__pagination {
+            bottom: -2em;
+          }
+          @media (min-width: 1024px) {
+            .splide__pagination {
+              bottom: -4em;
+              left: -135%;
+            }
           }
         `}
       </style>
